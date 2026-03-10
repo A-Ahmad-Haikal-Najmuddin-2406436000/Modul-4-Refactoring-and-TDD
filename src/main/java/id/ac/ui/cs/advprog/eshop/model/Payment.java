d6af413e5b4040bf676c7db6cd18fddbfabdca1d
Kallln <ahmad.haikal41@ui.ac.id>
2026-03-10 15:54:59 +0700

    [GREEN] Implements Payment model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -3,6 +3,7 @@ package id.ac.ui.cs.advprog.eshop.model;
 import lombok.Getter;
 import lombok.Setter;
 import java.util.Map;
+import java.util.Set;
 
 @Getter @Setter
 public class Payment {
@@ -12,9 +13,29 @@ public class Payment {
     private Map<String, String> paymentData;
 
     public Payment(String id, String method, Map<String, String> paymentData) {
-
+        if (id == null || id.trim().isEmpty()) {
+            throw new IllegalArgumentException("ID cannot be null or empty string");
+        }
+        if (method == null || method.trim().isEmpty()) {
+            throw new IllegalArgumentException("Method cannot be null or empty string");
+        }
+        if (paymentData == null) {
+            throw new IllegalArgumentException("Payment Data cannot be null");
+        }
+        this.id  = id;
+        this.method = method;
+        this.paymentData = paymentData;
     }
 
     public Payment(String id, String method, Map<String, String> paymentData, String status) {
+        this(id, method, paymentData);
+        this.status = status;
+    }
+
+    public void setStatus(String status) {
+        if (!Set.of("SUCCESS", "REJECTED", "PENDING").contains(status)) {
+            throw new IllegalArgumentException("Invalid status");
+        }
+        this.status = status;
     }
 }
